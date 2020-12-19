49c7ed2fa4ad7c32764ec6c7bbdb0c19cb49ca6b
KappaBull <kappa8v11@gmail.com>
2020-12-19 18:52:24 +0900

    GAEのデプロイ周りを整備

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/torebuild/site
+
+go 1.15
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	google.golang.org/appengine v1.6.7
+)
---
+++ setup.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/appengine"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.Static("/asset", "./asset")
+	r.Static("/images", "./images")
+
+	r.GET("/", func(c *gin.Context) {
+		//SafeHeaders
+		c.Header("X-Content-Type-Options", "nosniff")
+		c.Header("X-XSS-Protection", "1; mode=block")
+		c.Header("X-Frame-Options", "DENY")
+		c.Header("Strict-Transport-Security", "max-age=2592000; includeSubDomains")
+		//キャッシュ
+		now := time.Now().AddDate(0, 0, 1)
+		c.Header("Cache-Control", "max-age=300, public")
+		c.Header("Last-Modified", now.Format(http.TimeFormat))
+	})
+
+	r.Run()
+	appengine.Main()
+}
